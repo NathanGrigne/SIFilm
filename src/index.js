dd19f5ec38a087ddf4e53fb46575342e9694ab9a
nathangrigne <grigne.nathan@gmail.com>
2019-10-08 18:23:54 +0200

    oui

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Reset default styles
+import 'normalize.css'
+
+// Standard CSS import
+import './css/main.css'
+
+// You can also use SASS by adding this:
+// import './sass/main.sass'
+
+// Import your JS like so
+import TestClass from './js/TestClass'
+
+new TestClass()
