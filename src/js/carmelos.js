dfa8d9b0f8943ff38d4832e35577464f5159788c
Aurelien Sarrat <aurelien.sarrat@gmail.com>
2019-10-10 17:47:28 +0200

    Test for Password in Carmelos.html

--- src/js/carmelos.js
+++ src/js/carmelos.js
@@ -1,25 +1,47 @@
+// Video //
+
 const video = document.querySelector('.videoTrailer')
 video.addEventListener('click', ()=>{
     video.muted = false
     video.volume = 1
 })
 
-const consoleInputBegin = document.querySelector('.console_begin_box')
-const help = document.querySelector('.help')
+// Password //
+
+const password = document.querySelector('.classified')
 let value = null
-const password_box = document.querySelector('.console_password')
+password.addEventListener('keydown', function(_event){
+    value =  password.value
 
+    if(_event.key === 'Enter' && value === 'police' ){
+        document.createElement("div")
+        document.createElement("button")
+        document.createElement('a')
+        button.setAttribut('type','button')
+        button.innerText = 'Click Me'
+        div.appendChild('button')
+        button.appendChild('a')
+        a.setAttribut('href','../pages/seeTheTruth.html')
 
-consoleInputBegin.addEventListener('keydown', function(_event)
-{
-    value = consoleInputBegin.value
-
-    if(_event.key === 'Enter' && value === '/help')
-    {
-        help.style.display = 'block'
-        
     }
-    if(_event.key === 'Enter' &&  value ===  'pwd'){
-        password_box.style.display = 'flex'
+    else if(_event.key === 'Enter' && value === 'POLICE'){
+      document.createElement("div")
+      document.createElement("button")
+      document.createElement('a')
+      button.setAttribut('type','button')
+      button.innerText = 'Click Me'
+      div.appendChild('button')
+      button.appendChild('a')
+      a.setAttribut('href','../pages/seeTheTruth.html')
     }
+    else(_event.key === 'Enter' && value === 'Police')
+      document.createElement("div")
+      document.createElement("button")
+      document.createElement('a')
+      button.setAttribut('type','button')
+      button.innerText = 'Click Me'
+      div.appendChild('button')
+      button.appendChild('a')
+      a.setAttribut('href','../pages/seeTheTruth.html')
+
 })
