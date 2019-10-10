3ce6fa66d70dd7d801c2563bd6aa195311f0e6c5
nathangrigne <grigne.nathan@gmail.com>
2019-10-10 13:25:23 +0200

    update files

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,3 @@
-const video = document.querySelector('.videoTrailer')
-video.addEventListener('click', ()=>{
-    video.muted = false
-    video.volume = 1
-})
-
 // Timer
 
 let currentD = 0
@@ -34,19 +28,9 @@ function decompte(){
   }, 1000);
 }
 
-//Random Image Animation
-
-const img = document.querySelectorAll("#dossier img")
-const folder = document.querySelector("#dossier")
-
-for (let i = 0; i < img.length; i++) {
-  setTimeout(function(){
-     const angle = (Math.random() - 0.5) * 30
-
-     img[i].classList.toggle('animate')
-     img[i].style.transform = 'scale(1) rotate('+ angle +'deg)'
-     img[i].style.zIndex = i + 1
-     img[i].style.top = (Math.random() - 0.5) * 50 + 'vh'
-     img[i].style.left = (Math.random() - 0.5) * 90 + 'vw'
-  },(i+1)*750)
-}
+const fightIco = document.querySelector('#fight')
+const safeIco = document.querySelector('#safe')
+const warIco = document.querySelector('#war')
+const carIco = document.querySelector('#car')
+const camIco = document.querySelector('#cam')
+const twitchIco = document.querySelector('#twitch')
